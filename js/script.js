1ce4fa9a967e2584afac95111e12a94e3dd96aeb
cromeo98 <romeo.christian98@gmail.com>
2021-06-22 16:48:30 +0200

    add: imgs[0] with vue + alt

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            
+            imgs:[
+                'praia-da-falesia.jpg',
+                'praia-da-marinha.jpg',
+                'praia-da-rocha.jpg',
+                'praia-de-dona-ana.jpg',
+                'praia-do-camilo.jpg'
+            ],
+            alts:[
+                'praia da falesia',
+                'praia da marinha',
+                'praia da rocha',
+                'praia de dona ana',
+                'praia do camilo'
+            ],
         }
     }
 );
