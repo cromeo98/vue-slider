b01912e84667ab0941f7e6bc889f016264c2121b
cromeo98 <romeo.christian98@gmail.com>
2021-06-22 16:29:44 +0200

    add: basic layout

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // Descrizione: Rifare l'esercizio dello slider come fatto assieme in classe.
 // Bonus: Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            
+        }
+    }
+);
